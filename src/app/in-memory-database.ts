52f576d0c105e86f5afee6d4309ea49eac27a99a
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-04-03 11:50:51 -0300

    Inclusao do modelo de Categoria.

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,10 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { Category } from './pages/categories/shared/category.model';
+
 export class InMemoryDatabase implements InMemoryDbService{
   createDb() {
-      const categories = [
+      const categories: Category[] = [
           { id: 1, name: 'Lazer', description: 'Cinema, Praia, Parque, etc' },
           { id: 2, name: 'Saúde', description: 'Plano de Saúde, Consultas, Exames' },
           { id: 3, name: 'Moradia', description: 'Aluguel, IPTU' },
