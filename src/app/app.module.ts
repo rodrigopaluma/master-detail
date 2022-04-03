627128bee6e7285c5ee1da9f7af22064ffaeccd8
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-04-03 12:15:55 -0300

    Interceptaçao das rwquisiçoes http pelo InMemoryDatabase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Banco Temporário
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from './in-memory-database';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // Banco Temporário
   ],
   providers: [],
   bootstrap: [AppComponent]
