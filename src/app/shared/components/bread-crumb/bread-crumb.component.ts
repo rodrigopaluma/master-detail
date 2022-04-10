7c0a4d3c20a65db63188a44e2e8ff5134545604c
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-04-10 11:33:36 -0300

    Criação do app-bread-crumb.

---
+++ src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+interface BreadCrumbItem {
+  label: string;
+  link?: string;
+}
+
+@Component({
+  selector: 'app-bread-crumb',
+  templateUrl: './bread-crumb.component.html',
+  styleUrls: ['./bread-crumb.component.scss']
+})
+export class BreadCrumbComponent implements OnInit {
+
+  @Input() items: Array<any> = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isTheLastItem(item: BreadCrumbItem): boolean {
+    const index = this.items.indexOf(item);
+    return index + 1 === this.items.length;
+
+  }
+
+}
