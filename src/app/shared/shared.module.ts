8bd15181a11f912f91b6c42d762a276b4628677e
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-04-10 12:12:14 -0300

    Criação do PageHeaderComponent e implementação em todas as telas da aplicação.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
 import { RouterModule } from '@angular/router';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 
 @NgModule({
-  declarations: [BreadCrumbComponent],
+  declarations: [BreadCrumbComponent, PageHeaderComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -15,7 +16,8 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     ReactiveFormsModule,
     BreadCrumbComponent,
-    RouterModule
+    RouterModule,
+    PageHeaderComponent
   ]
 })
 export class SharedModule { }
