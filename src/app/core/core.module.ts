d7538adddf4270f01c67cd5fc952d84c03fb12d8
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-04-11 07:59:25 -0300

    Extração do Navbar.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,24 +3,28 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 // Banco Temporário
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase), // Banco Temporário
+    RouterModule
   ],
   exports: [ // Módulos Compartilhados
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
