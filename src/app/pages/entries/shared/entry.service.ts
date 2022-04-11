8a846c78a311a237c313ecf6492e909f9338a14d
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-04-11 11:28:34 -0300

    Implementação do relatório de Despesas e Receitas.

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,7 +3,9 @@ import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { Observable } from 'rxjs';
-import { catchError, flatMap } from 'rxjs/operators';
+import { catchError, flatMap, map } from 'rxjs/operators';
+
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +35,25 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handleError)
     );
+  }
+
+  // Geração de Relatório
+  public getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    );
+  }
+
+  // Filtro por Mês e Ano
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number): Entry[] {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if (monthMatches && yearMatches)
+        return entry;
+    });
   }
 
 }
